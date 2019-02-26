9ddf883fd38d34b022404c8e1da0cb53af91b4c8
AvenueDuWeb <contact@avenueduweb.net>
2019-02-26 14:51:29 +0100

    Update review.js

--- review.js
+++ review.js
@@ -1,10 +1,16 @@
-$('#multi-ratings .js--film-ratings').children().map(function(n) {
-    var $this = $(this),
-        rank = n + 1,
-        title = $this.find('.film-title').text().trim(),
-        dirYear = $this.find('.director-year').text().trim(),
-        re = dirYear.match(/^(.+), (\d+)$/),
-        dir = re ? re[1] : dirYear,
-        year = re ? re[2] : '';
-    return [rank, title, year, dir].join('\t');
+var movies = null;
+if($('.js--film-ratings').length > 0) {
+    movies =  $('.js--film-ratings').children();
+} else {
+    movies = $('.film-grid').children();
+}
+movies.map(function(n) {
+var $this = $(this),
+    rank = ($this.find('.list-film-position').text().length > 0 ? parseInt($this.find('.list-film-position').text().trim()) : n + 1),
+    title = $this.find('.film-title').text().trim(),
+    dirYear = $this.find('.director-year').text().trim(),
+    re = dirYear.match(/^(.+), (\d+)$/),
+    dir = re ? re[1] : dirYear,
+    year = re ? re[2] : '';
+return [rank, title, year, dir].join('\t');
 }).get().join('\n');
