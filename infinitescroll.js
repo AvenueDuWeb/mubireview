8de6a2d4f3748af35db727de5143682513f6edd8
AvenueDuWeb <contact@avenueduweb.net>
2019-02-26 14:31:01 +0100

    Update infinitescroll.js

--- infinitescroll.js
+++ infinitescroll.js
@@ -1,4 +1,4 @@
 var handle = setInterval(function() {
-if ($('.js--film-ratings').children().length < parseInt($('#profile-nav .active .nav-stat').text().replace(/,/g , ''))) {
+if ($('body').find('.media').length < parseInt($('#profile-nav .active .nav-stat').text().replace(/,/g , ''))) {
     $("html").scrollTop($(".app-load-more").offset().top);
 } else {clearInterval(handle)}}, 1000);
