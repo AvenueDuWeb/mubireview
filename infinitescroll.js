7ef515cd106f4567daf35eac0ef4920ddc60cc20
AvenueDuWeb <contact@avenueduweb.net>
2019-02-26 15:26:49 +0100

    Update infinitescroll.js

--- infinitescroll.js
+++ infinitescroll.js
@@ -1,14 +1,11 @@
-var handle = setInterval(function() {
+var handle = setInterval(function(countMovies) {
 if($('.js--film-ratings').length > 0) {
     movies =  $('.js--film-ratings');
 } else {
     movies = $('.film-grid');
 }
-if (movies.children().length < parseInt($('#profile-nav .active .nav-stat').text().replace(/,/g , ''))) {
-    var originHeight = movies.height();
-    setTimeout(function() { $('.app-load-more').click(); }, 1000);
-    var originHeight = movies.height();
-    if(originHeight === newHeight) {
-        $("html").scrollTop($("#hs-beacon").offset().top);
-    }
+
+if (movies.children().length < parseInt($('.nav-with-stats .active .nav-stat').text().replace(/,/g , ''))) {
+    $("html").scrollTop($("#hs-beacon").offset().top);
+    $('.app-load-more').click(); 
 } else {clearInterval(handle)}}, 1000);
